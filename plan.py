936be223f1a8151b8a63f842bab4b9e9ebfe113e
Paúl Herrera <pauljherrera@gmail.com>
2018-02-07 17:54:26 -0300

    first commit

---
+++ plan.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import datetime as dt
+
+
+
+config = {
+    # General config.
+    'start_data': dt.datetime(2018, 1, 1),
+    'end_date': dt.datetime(2018, 2, 1),
+
+    # Strategy config.
+    'ticker': 'GOOG',
+}
+
+
+
+class DataDownloader:
+    pass
+
+
+class DataFeeder:
+    def __init__(self, strategy=None, *args, **kwargs):
+        self.strategy = strategy
+        
+        
+    def feed_data():
+        for i in self.data.values:
+            # Filter data to be sent.
+            
+            # Updating the strategy.
+            self.strategy.update(data)
+    
+    
+class StrategyBase:
+    """
+    Abstract base class for strategies.
+    """
+    def update(data):
+        raise NotImplementedError
+        
+
+class Strategy(StrategyBase):
+    def update(data):
+        # Strategy logic.
+        pass
+    
+
+    
+    
+class OptionsTrader():
+    def __init__(self, *args, **kwargs):
+        # Creating dataframe for logging trades.
+        columns = ['Ticker', 'Price', 'Side', 'Quantity'] # Date will be the index.
+        self.asset_log = pd.DataFrame(columns=columns)
+
+        columns = ['Ticker', 'Price', 'Side', 'Type', 'expirDate',
+                   'strike', 'Quantity']                  # Date will be the index.  
+        self.options_log = pd.DataFrame(columns=columns)
+        
+        
+    def buy_asset():
+        pass
+    
+    def sell_asset():
+        pass
+    
+    def buy_call():
+        pass
+    
+    def buy_put():
+        pass
+    
+    def sell_call():
+        pass
+    
+    def sell_put():
+        
+        
+
+
---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import quandl
+
+quandl.ApiConfig.api_key = "7MA978mgAH8cTzL7CGa_"
+quandl.ApiConfig.page_limit=300
+
+database_name = 'OSMV'
+db = quandl.Database('OSMV')
+
+counter = 1
+full_list = []
+
+flag = True
+while flag:
+    all_db = db.all(params={'page': 2 })
+
+
+def load_data():
+    return pd.read_csv('data/OSMV.csv')
+
+df = load_data()
