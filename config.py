5e0e1a48d9c1be68d9016cc37103968f50bb5232
Luis Omar <32350181+luisomar3@users.noreply.github.com>
2018-02-13 21:33:15 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ config = {
     # General config.
     'start_date': '2015-11-16',
     'end_date': '2015-11-20',
-    'api_key' : "7MA978mgAH8cTzL7CGa_",
+    'api_key' : "",
 
     # Strategy config.
     'ticker': 'GOOG',
