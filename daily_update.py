7e0b05d5e7a78dc693584e3671006b3d195986fa
luis <luisomar@hotmail.com>
2018-03-20 23:10:27 -0400

    tets

---
+++ daily_update.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from config import config
+from data.datadownloader import DataDownloader
+
+downloader = DataDownloader()
+
+
+
+
+def main():
+   
+   downloader.daily_update()
+
+if __name__ == '__main__':
+    main()
