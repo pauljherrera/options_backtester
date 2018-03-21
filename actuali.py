bd14efd23fdb579aa32721fe4eec0954ef6cf414
luis <luisomar@hotmail.com>
2018-03-21 00:35:46 -0400

    last two missing dates

--- actuali.py
+++ actuali.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 from config import config
-from data.datafeeder import DataFeeder
+from data.datafeeder import DataDownloader
 
-downloader = DataFeeder()
+downloader = DataDownloader()
 
 
 
 
 def main():
    
-   downloader.checker()
+   downloader.load_multiple_files('options_backtester','OSMV_TABLES')
 
 if __name__ == '__main__':
     main()
