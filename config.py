b2ffc9b6d0ea2255743ca338d200ca15acefe19a
luis <luisomar@hotmail.com>
2018-03-20 23:03:24 -0400

    config

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ config = {
 
     # Strategy config.
     'ticker': 'GOOGL',
-    'strategy':'Protective Put',
+    'strategy':'covered call',
     'fillPrice':'Bid', #Bid or Ask
     'premiun' : 0.5,
-    '%OTM' : -2,
+    '%OTM' : 4.5,
     'frecuency' : 30, #Expresed in days
     'duration' : 30, #Expresed in years
-    'initial_position':1000,
+    'initial_position':10000,
     '%ofPosition':100,
     'exchange_comisions':1,
     'buy/sell stock':0 #Number of stocks bought for every trade
